8012c4815298526710fcc274523391165e7711e6
Evejweinberg <evejweinberg@gmail.com>
2016-11-22 23:10:30 -0500

    hovering works for all json ele,ents and added color

--- js/people.js
+++ js/people.js
@@ -1,5 +1,5 @@
 var people = [
-  
+
   {
   },
 
@@ -10,8 +10,9 @@ position: "President-Elect (Confirmed)",
 info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
 url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
 top: "79%",
-left: "20%",
-age: "70"
+left: "80%",
+age: "70",
+color: "#b7b7b7"
 
 },
 {
@@ -20,8 +21,9 @@ age: "70"
   info: "History of ANTI-LGBT and ANTI-ABORTION LEGISLATION. Introduced a bill to END FEDERAL FUNDING FOR PLANNED PARENTHOOD. Believes in CONVERSION THERAPY.",
   url: "https://libraries.indiana.edu/sites/default/files/2009-06-04_dlb_0126.jpg",
   top: "30%",
-  left: "29%",
-  age: "57"
+  left: "69%",
+  age: "57",
+  color: "#c6c6c6"
 
 },
 {
@@ -29,9 +31,10 @@ age: "70"
   position: "Chief Strategist of the White House (Confirmed)",
   info: "Former chairman of BREITBART NEWS,'The platform for the ALT-RIGHT,' a group known for WHITE-NATIONALIST and ANTI SEMETIC POLITICS. Celebrated by the KKK and the AMERICAN NAZI PARTY. Ex-Wife accused him of ANTI-SEMITISM",
   url: "https://peopledotcom.files.wordpress.com/2016/08/steve-bannon-435.jpg?w=435&h=580",
-  top: "9%",
+  top: "39%",
   left: "20%",
-  age: "62"
+  age: "62",
+  color: "#b0b0b0"
 
 },
 {
@@ -40,8 +43,9 @@ age: "70"
   info: "Has refered to Islam as a 'CANCER,' Fired by Obama Administration in 2014",
   url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
   top: "40%",
-  left: "20%",
-  age: "57"
+  left: "10%",
+  age: "57",
+  color: "#f4f233"
 
 },
 {
@@ -49,9 +53,10 @@ age: "70"
   position: "Attorney General (Confirmed)",
   info: "Called both the ACLU and the NAACP 'UN-AMERICAN.' Called a white civil rights lawyer a 'DISGRACE TO HIS RACE.' OPPOSES hate crime PROTECTIONS, marriage EQUALITY, and open military service for GAY and TRANSGENDERED people. ",
   url: "http://www.natlnarc.org/wp-content/uploads/2016/05/Jeff-Sessions.jpg",
-  top: "16%",
+  top: "66%",
   left: "20%",
-  age: "69"
+  age: "69",
+  color: "#c7b700"
 
 },
 {
@@ -61,7 +66,8 @@ age: "70"
   url: "http://bloximages.chicago2.vip.townnews.com/gctelegram.com/content/tncms/assets/v3/editorial/5/89/589aa458-6e28-5f05-a159-4480bd950be8/582fd242be363.image.jpg?resize=1200%2C1723",
   top: "69%",
   left: "40%",
-  age: "52"
+  age: "52",
+  color: "#cccccc"
 
 },
 {
@@ -69,9 +75,10 @@ age: "70"
   position: "White House Chief of Staff (Confirmed)",
   info: "Republican National Commitee Chairman since 2011 - Exactly the type of politician the President-elect ran his campaign against.",
   url: "https://pbs.twimg.com/profile_images/588448493892579328/CN351XjP.png",
-  top: "9%",
+  top: "19%",
   left: "30%",
-  age: "44"
+  age: "44",
+  color: "#b7b7b7"
 
 }
 ]
---
+++ readme.md
@@ -0,0 +1,15 @@
+change the size of the headshots in index.html, line 21:
+'5vw' is 5% of the browser's 'view width'
+
+.head{
+  position: absolute;
+  width: 5vw;
+  height: 5vw;
+  /*border: red solid 2px;*/
+  z-index: 10;
+}
+
+
+change the placement of the images in the people.js json file.
+
+add text color as hex codes in the people.js json
