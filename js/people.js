5f1676aa543721148b707410028265eb0edfb786
Evejweinberg <evejweinberg@gmail.com>
2016-11-22 22:31:32 -0500

    added corbons json

--- js/people.js
+++ js/people.js
@@ -1,56 +1,74 @@
 var people = [
 
 
-
 {
 name: 'Donald Trump',
 position: "President-Elect (Confirmed)",
 info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
 url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
-top: "9%",
+top: "79%",
 left: "20%",
 age: "70"
 
 },
 {
-  name: 'Donald Trump',
-  position: "President-Elect (Confirmed)",
-  info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
-  url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
-  top: "9%",
-  left: "20%",
-  age: "70"
+  name: 'Mike Pence',
+  position: "Vice President-Elect (Confirmed)",
+  info: "History of ANTI-LGBT and ANTI-ABORTION LEGISLATION. Introduced a bill to END FEDERAL FUNDING FOR PLANNED PARENTHOOD. Believes in CONVERSION THERAPY.",
+  url: "https://libraries.indiana.edu/sites/default/files/2009-06-04_dlb_0126.jpg",
+  top: "30%",
+  left: "29%",
+  age: "57"
 
 },
 {
-  name: 'Donald Trump',
-  position: "President-Elect (Confirmed)",
-  info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
-  url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
+  name: 'Steve Bannon',
+  position: "Chief Strategist of the White House (Confirmed)",
+  info: "Former chairman of BREITBART NEWS,'The platform for the ALT-RIGHT,' a group known for WHITE-NATIONALIST and ANTI SEMETIC POLITICS. Celebrated by the KKK and the AMERICAN NAZI PARTY. Ex-Wife accused him of ANTI-SEMITISM",
+  url: "https://peopledotcom.files.wordpress.com/2016/08/steve-bannon-435.jpg?w=435&h=580",
   top: "9%",
   left: "20%",
-  age: "70"
+  age: "62"
 
 },
 {
-  name: 'Donald Trump',
-  position: "President-Elect (Confirmed)",
-  info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
+  name: 'Ret. Lt. Gen. Mike Flynn',
+  position: "National Security Advisor (Confirmed)",
+  info: "Has refered to Islam as a 'CANCER,' Fired by Obama Administration in 2014",
   url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
-  top: "9%",
+  top: "40%",
   left: "20%",
-  age: "70"
+  age: "57"
 
 },
 {
-  name: 'Donald Trump',
-  position: "President-Elect (Confirmed)",
-  info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
-  url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
-  top: "9%",
+  name: 'Jeff Sessions',
+  position: "Attorney General (Confirmed)",
+  info: "Called both the ACLU and the NAACP 'UN-AMERICAN.' Called a white civil rights lawyer a 'DISGRACE TO HIS RACE.' OPPOSES hate crime PROTECTIONS, marriage EQUALITY, and open military service for GAY and TRANSGENDERED people. ",
+  url: "http://www.natlnarc.org/wp-content/uploads/2016/05/Jeff-Sessions.jpg",
+  top: "16%",
   left: "20%",
-  age: "70"
+  age: "69"
+
+},
+{
+  name: 'Mike Pompeo',
+  position: "CIA Director (Confirmed)",
+  info: "On Edward Snowden, 'I think that the proper outcome would be that he would be given a DEATH SENTENCE.' ",
+  url: "http://bloximages.chicago2.vip.townnews.com/gctelegram.com/content/tncms/assets/v3/editorial/5/89/589aa458-6e28-5f05-a159-4480bd950be8/582fd242be363.image.jpg?resize=1200%2C1723",
+  top: "69%",
+  left: "40%",
+  age: "52"
+
+},
+{
+  name: 'Reince Priebus',
+  position: "White House Chief of Staff (Confirmed)",
+  info: "Republican National Commitee Chairman since 2011 - Exactly the type of politician the President-elect ran his campaign against.",
+  url: "https://pbs.twimg.com/profile_images/588448493892579328/CN351XjP.png",
+  top: "9%",
+  left: "30%",
+  age: "44"
 
 }
-
 ]
