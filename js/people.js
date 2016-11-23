e864e75354b94e41f367eb686ee113cae285a2f1
Corbin Ordel <corbinordel@gmail.com>
2016-11-23 15:04:11 -0500

    Update people.js

--- js/people.js
+++ js/people.js
@@ -7,7 +7,7 @@ var people = [
 {
 name: 'Donald Trump',
 position: "President-Elect (Confirmed)",
-info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million",
+info: "13 women have accused him of SEXUAL ASSAULT, Settled Trump University FRAUD Lawsuit for $25 Million. Called global warming a hoax perpetrated by China",
 url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
 top: "79%",
 left: "80%",
