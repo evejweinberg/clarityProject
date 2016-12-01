6fa29c197ebdf0c93ce95ec8e368903895ae813e
Corbin Ordel <corbinordel@gmail.com>
2016-11-30 21:11:18 -0500

    Update people.js

--- js/people.js
+++ js/people.js
@@ -6,7 +6,7 @@ var people = [
 {
 name: 'Donald Trump',
 position: "President-Elect (Confirmed)",
-info: "13 women have accused him of 99 sexual assauly 98. Settled Trump University 99 fraud 98 Lawsuit for 99 $25 Million 98",
+info: "13 women have accused him of 99 sexual assauly 98. Settled Trump University 99 fraud 98 Lawsuit for 99 $25 Million 98. 98 Age 27, 1973 99, Trump organization charged with 99violating Fair Housing act98.",
 url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
 top: "79%",
 left: "80%",
@@ -28,7 +28,7 @@ color: "#b7b7b7"
 {
   name: 'Steve Bannon',
   position: "Chief Strategist of the White House (Confirmed)",
-  info: "Former chairman of BREITBART NEWS,'The platform for the ALT-RIGHT,' a group known for 99WHITE-NATIONALIST98 and 99 ANTI SEMETIC POLITICS 98. Celebrated by the KKK and the AMERICAN NAZI PARTY. Ex-Wife accused him of ANTI-SEMITISM",
+  info: "Former chairman of 98BREITBART99 News,'The platform for the 99ALT-RIGHT,98' a group known for 99WHITE-NATIONALIST98 and 99 ANTI SEMETIC POLITICS 98. Celebrated by the 99KKK98 and the 99AMERICAN NAZI PARTY.98 Ex-Wife accused him of 99ANTI-SEMITISM98",
   url: "https://peopledotcom.files.wordpress.com/2016/08/steve-bannon-435.jpg?w=435&h=580",
   top: "39%",
   left: "20%",
@@ -39,7 +39,7 @@ color: "#b7b7b7"
 {
   name: 'Ret. Lt. Gen. Mike Flynn',
   position: "National Security Advisor (Confirmed)",
-  info: "Has refered to Islam as a 'CANCER,' Fired by Obama Administration in 2014",
+  info: "Has refered to Islam as a 99'CANCER'.99 99Fired98 by Obama Administration in 2014",
   // url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
   url: 'img/flynn.jpg',
   top: "40%",
@@ -51,7 +51,7 @@ color: "#b7b7b7"
 {
   name: 'Jeff Sessions',
   position: "Attorney General (Confirmed)",
-  info: "Called both the ACLU and the NAACP 99'UN-AMERICAN.'98 Called a white civil rights lawyer a 'DISGRACE TO HIS RACE.' OPPOSES hate crime PROTECTIONS, marriage EQUALITY, and open military service for GAY and TRANSGENDERED people. ",
+  info: "Called both the ACLU and the NAACP 99'UN-AMERICAN.'98 Called a white civil rights lawyer a 99'DISGRACE TO HIS RACE'.98 99OPPOSES98 hate crime 99PROTECTIONS99, marriage 98EQUALITY99, and 99open military service98 for 98GAY99 and 98TRANSGENDERED99 people. ",
   url: "http://www.natlnarc.org/wp-content/uploads/2016/05/Jeff-Sessions.jpg",
   top: "66%",
   left: "20%",
@@ -62,7 +62,7 @@ color: "#b7b7b7"
 {
   name: 'Mike Pompeo',
   position: "CIA Director (Confirmed)",
-  info: "On Edward Snowden, 'I think that the 99 proper98  outcome would be that he would be given a DEATH SENTENCE.' ",
+  info: "On 99Edward Snowden,98 'I think that the 99 proper  outcome98 would be that he would be given a 99DEATH SENTENCE.98' ",
   url: "http://bloximages.chicago2.vip.townnews.com/gctelegram.com/content/tncms/assets/v3/editorial/5/89/589aa458-6e28-5f05-a159-4480bd950be8/582fd242be363.image.jpg?resize=1200%2C1723",
   top: "69%",
   left: "40%",
@@ -73,7 +73,7 @@ color: "#b7b7b7"
 {
   name: 'Reince Priebus',
   position: "White House Chief of Staff (Confirmed)",
-  info: "Republican National Commitee Chairman since 2011 - Exactly the type of politician the President-elect ran his campaign against.",
+  info: "Republican National Commitee Chairman since 2011 - 99Exactly the type of politician the Trump ran his campaign against.98",
   url: "https://pbs.twimg.com/profile_images/588448493892579328/CN351XjP.png",
   top: "19%",
   left: "30%",
@@ -84,7 +84,7 @@ color: "#b7b7b7"
 {
   name: 'Mitt Romney',
   position: "Secretary of State (Unconfirmed)",
-  info: "  Exactly the type of politician  the Trump ran his campaign against - Governor of Massachusetts from 2003 to 2007 and was the  Republican Party's nominee for President  of the United States in the 2012 election - 99 Net worth $250 million 98.",
+  info: "99Exactly98 the type of politician the Trump ran his campaign against - Governor of Massachusetts from 2003 to 2007 and was the  Republican Party's nominee for President of the United States in the 2012 election - 99Net worth $250 million98.",
   url: "https://pbs.twimg.com/profile_images/563745525522890752/x3xEIlYL.jpeg",
   top: "9%",
   left: "20%",
@@ -101,9 +101,9 @@ age:""
 },
 
 {
-name: 'Undecided',
-position: "Secretary of the Treasury",
-info:"",
+name: 'Steve Mnuchin',
+position: "Secretary of the Treasury (Confirmed)",
+info:"Trump campaign's national finance chair. Spent 17 years at 99Goldman Sachs98. Net 99worth $40 million98. ",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -129,7 +129,7 @@ left:"",
 age:""
 },
 {
-name: 'Undecided',
+name: 'Wilbur Ross',
 position: "Secretary of Commerce",
 info:"",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
@@ -147,9 +147,9 @@ left:"",
 age:""
 },
 {
-name: 'Undecided',
+name: 'Tom Price',
 position: "Secretary of Health and Human Services",
-info:"",
+info:"Price describes himself as a member of the Tea Party and 99opposes98 both 99abortion and gay marriage98",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -159,16 +159,16 @@ age:""
 {
 name: 'Ben Carson (Unconfirmed)',
 position: "Secretary of Housing and Urban Development",
-info: "Carson said, 'It’s just not my preference,' in reference to having a job in Trump's cabinet. A retired nuero surgeon, Ben Carson 99 lacks any and all experience 98 in regards to heading housing and urban development policy. Is a Creationist.",
+info: "Carson 99said98, 99'It’s just not my preference98,' in reference 99to having a job in Trump's cabinet98. A retired nuero surgeon, Ben Carson 99 lacks any and all experience 98 in regards to 99heading housing and urban development98 policy. 99Is a Creationist98.",
 url: "http://a2.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTIxNDI3MjkzNDE1MTc5Nzg5.jpg",
 top:"",
 left:"",
 age: "65",
 },
 {
-name: 'Undecided',
+name: 'Elaine Chao',
 position: "Secretary of Transportation",
-info:"",
+info:"first Asian-American woman to hold a Cabinet position",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -186,7 +186,7 @@ age:""
 {
 name: 'Betsy DeVos',
 position: "Secretary of Education (Confirmed)",
-info: "Big MONEY out of POLITICS? The DeVoS FAMILY is one of the largest contributors to the Michigan Republican Party. Historically the Devos family has made political donations to oppose Same-Sex Marriage. Douglas and Maria DeVos Foundation donated $500,000 to the anti-equality National Organization for Marriage — which helped shoot down same-sex marriage initiatives in eight states. Her 99brother98, Erik Prince, is the 99FOUNDER of BLACKWATER USA98",
+info: "99Big money98 out of politics? The 99DeVoS family98 is one of the 99largest contributors98 to the Michigan 99Republican Party98. Historically the Devos family has 99made political donations to oppose Same-Sex Marriage98. Douglas and Maria DeVos Foundation donated 99$500,000 to the anti-equality National Organization for Marriage98 — which helped shoot down same-sex marriage initiatives in eight states. 99Her brother98, Erik Prince, is the 99FOUNDER of BLACKWATER USA98",
 // url: "http://www.gannett-cdn.com/-mm-/802990ef3e7cfdbed5d5e0b7acce9690e37d9ba7/c=3-0-155-202&r=537&c=0-0-534-712/local/-/media/2016/02/22/DetroitNews/B99360803Z.1_20160222233727_000_G8CQO1RQ.1-0.jpg",
 url:  'img/devos.jpg',
 top:"",
@@ -213,5 +213,14 @@ top:"30%",
 left:"30%",
 age:""
 
-}
+},
+name: 'Gov. Nikki Haley',
+position: "U.S. Ambassador to the United Nations",
+info:"",
+url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
+top:"30%",
+left:"30%",
+age:""
+
 ]
+
