e40afee890e265a18210b13b41f0874364552a9b
Corbin Ordel <corbinordel@gmail.com>
2016-12-08 21:21:28 -0500

    Update people.js

--- js/people.js
+++ js/people.js
@@ -1,11 +1,9 @@
 var people = [
 
-
-
-
 {
 name: 'Donald Trump',
 position: "President-Elect (Confirmed)",
+job description: "As Commander in Chief, has the authority to send troops into combat, and is the only one who can decide whether to use nuclear weapons. As chief executive, he enforces laws, treaties, and court rulings; develops federal policies; prepares the national budget; and appoints federal officials. Approves or vetoes acts of Congress and grants pardons.",
 info: "13 women have accused him of 99 sexual assauly 98. Settled Trump University 99 fraud 98 Lawsuit for 99 $25 Million 98. 98 Age 27, 1973 99, Trump organization charged with 99violating Fair Housing act98.",
 url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
 top: "79%",
@@ -17,7 +15,8 @@ color: "#b7b7b7"
 {
   name: 'Mike Pence',
   position: "Vice President-Elect (Confirmed)",
-  info: "History of 98 ANTI-LGBT 99 and 98 ANTI-ABORTION 99 legislation. Introduced a bill to 99 END FEDERAL FUNDING FOR PLANNED PARENTHOOD 98. Believes in 99 CONVERSION THERAPY 98.",
+  job description: "Presiding officer of the Senate. But the Vice President also serves as ceremonial assistant to the President",
+  info: "History of 99 ANTI-LGBT 98 and 99 ANTI-ABORTION 98 legislation. Introduced a bill to 99 END FEDERAL FUNDING FOR PLANNED PARENTHOOD 98. Believes in 99 CONVERSION THERAPY 98.",
   url: "https://libraries.indiana.edu/sites/default/files/2009-06-04_dlb_0126.jpg",
   top: "30%",
   left: "69%",
@@ -28,7 +27,8 @@ color: "#b7b7b7"
 {
   name: 'Steve Bannon',
   position: "Chief Strategist of the White House (Confirmed)",
-  info: "Former chairman of 98BREITBART99 News,'The platform for the 99ALT-RIGHT,98' a group known for 99WHITE-NATIONALIST98 and 99 ANTI SEMETIC POLITICS 98. Celebrated by the 99KKK98 and the 99AMERICAN NAZI PARTY.98 Ex-Wife accused him of 99ANTI-SEMITISM98",
+  job description: "Strategise on the day to day decisions of the President as well as looking at  think strategically about the president's entire tenure.",
+  info: "Former chairman of 99BREITBART98 News,'The platform for the 99ALT-RIGHT,98' a group known for 99WHITE-NATIONALIST98 and 99 ANTI SEMETIC POLITICS 98. Celebrated by the 99KKK98 and the 99AMERICAN NAZI PARTY.98Ex-Wife accused him of 99ANTI-SEMITISM99.",
   url: "https://peopledotcom.files.wordpress.com/2016/08/steve-bannon-435.jpg?w=435&h=580",
   top: "39%",
   left: "20%",
@@ -39,7 +39,8 @@ color: "#b7b7b7"
 {
   name: 'Ret. Lt. Gen. Mike Flynn',
   position: "National Security Advisor (Confirmed)",
-  info: "Has refered to Islam as a 99'CANCER'.99 99Fired98 by Obama Administration in 2014",
+  job description: "Serves as a chief advisor to the President of the United States on national security issues.",
+  info: "Has refered to Islam as a 99'CANCER'98. 99Fired98 by Obama Administration for ongoing conflicts in 2014",
   // url: "http://i2.cdn.cnn.com/cnnnext/dam/assets/160309205615-donald-trump-islam-intv-ac-cooper-sot-00001209-large-169.jpg",
   url: 'img/flynn.jpg',
   top: "40%",
@@ -51,7 +52,8 @@ color: "#b7b7b7"
 {
   name: 'Jeff Sessions',
   position: "Attorney General (Confirmed)",
-  info: "Called both the ACLU and the NAACP 99'UN-AMERICAN.'98 Called a white civil rights lawyer a 99'DISGRACE TO HIS RACE'.98 99OPPOSES98 hate crime 99PROTECTIONS99, marriage 98EQUALITY99, and 99open military service98 for 98GAY99 and 98TRANSGENDERED99 people. ",
+  job description: "Chief law enforcement officer and chief lawyer of the United States government.",
+  info: "99Called98 both the 99ACLU98 and the 99NAACP98 99'UN-AMERICAN'98.'Called a white civil rights lawyer a 99'DISGRACE98 TO HIS 99RACE'98. 99OPPOSES98 hate crime 99PROTECTIONS98, marriage 99EQUALITY98, and 99open military service98 for 99GAY98 and 99TRANSGENDERED98 people.",
   url: "http://www.natlnarc.org/wp-content/uploads/2016/05/Jeff-Sessions.jpg",
   top: "66%",
   left: "20%",
@@ -62,7 +64,8 @@ color: "#b7b7b7"
 {
   name: 'Mike Pompeo',
   position: "CIA Director (Confirmed)",
-  info: "On 99Edward Snowden,98 'I think that the 99 proper  outcome98 would be that he would be given a 99DEATH SENTENCE.98' ",
+  job description: "To serve as the head of the Central Intelligence Agency and report to the Director of National Intelligence. "
+  info: "On 99Edward Snowden,98 'I think that the 99proper outcome98 would be that he would be given a 99DEATH SENTENCE.98' ",
   url: "http://bloximages.chicago2.vip.townnews.com/gctelegram.com/content/tncms/assets/v3/editorial/5/89/589aa458-6e28-5f05-a159-4480bd950be8/582fd242be363.image.jpg?resize=1200%2C1723",
   top: "69%",
   left: "40%",
@@ -73,6 +76,7 @@ color: "#b7b7b7"
 {
   name: 'Reince Priebus',
   position: "White House Chief of Staff (Confirmed)",
+  job description: "The duties of the White House Chief of Staff vary greatly in each administration, according to the needs and desires of each president. The position typically plays both a managerial and advisory role.",
   info: "Republican National Commitee Chairman since 2011 - 99Exactly the type of politician the Trump ran his campaign against.98",
   url: "https://pbs.twimg.com/profile_images/588448493892579328/CN351XjP.png",
   top: "19%",
@@ -80,20 +84,31 @@ color: "#b7b7b7"
   age: "44",
   color: "#b7b7b7"
 },
-
 {
-  name: 'Mitt Romney',
+  name: 'David Petraeus',
   position: "Secretary of State (Unconfirmed)",
-  info: "99Exactly98 the type of politician the Trump ran his campaign against - Governor of Massachusetts from 2003 to 2007 and was the  Republican Party's nominee for President of the United States in the 2012 election - 99Net worth $250 million98.",
+  job description: "Carries out the President’s foreign policies through the State Department and the Foreign Service of the United State.",
+info:"In 2015, Petraeus was convicted of mishandling classified material by sharing documents with his then lover, Paula Broadwell, knowing it contained classified information. His lawyers kept him out of prison by negotiating a plea deal with the Justice Department.",
+hypocrisy rating: "10/10 - During the presidential campaign, Trump continually demonized Hillary Clinton for her use of private e-mails as secretary of state and called for her imprisonment. "
   url: "https://pbs.twimg.com/profile_images/563745525522890752/x3xEIlYL.jpeg",
   top: "9%",
   left: "20%",
   age: "69",
 },
+// {
+//   name: 'Mitt Romney',
+//   position: "Secretary of State (Unconfirmed)",
+//   info: "99Exactly98 the type of politician the Trump ran his campaign against - Governor of Massachusetts from 2003 to 2007 and was the  Republican Party's nominee for President of the United States in the 2012 election - 99Net worth $250 million98.",
+//   url: "https://pbs.twimg.com/profile_images/563745525522890752/x3xEIlYL.jpeg",
+//   top: "9%",
+//   left: "20%",
+//   age: "69",
+// },
 {
-name: 'Undecided',
+name: 'James Mattis',
 position: "Secretary of Defense",
-info: "",
+job description: "Under the direction of the President, the Secretary exercises authority, direction, and control over the Department of Defense."
+info: "Believes torture is un-effective 'I don't find waterboarding useful'. His appointment breaks from a long standing tradition of not naming a retired military leader as defense secretary. He will require a waver from congress that will allow for him to serve in this position, as he has only been out of active military service for 3 years as opposed to the required 7.",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top: "",
 left:"",
@@ -103,7 +118,9 @@ age:""
 {
 name: 'Steve Mnuchin',
 position: "Secretary of the Treasury (Confirmed)",
-info:"Trump campaign's national finance chair. Spent 17 years at 99Goldman Sachs98. Net 99worth $40 million98. ",
+job description: "To act as the principal economic advisor to the President and play a critical role in policy-making by bringing an economic and government financial policy perspective to issues facing the government.",
+info:"Trump campaign's national finance chair. Spent 17 years at 99Goldman Sachs98. Net 99worth $40 million98.  After the financial crisis, Mnuchin found a way to make money off the depressed real estate market. He took over OneWest Bank, which bought up and foreclosed on de-valued homes — especially in low income minority neighborhoods.",
+hipocracy rating: "10/10 -  Trump Campain ad said Goldman Sachs ‘robbed our working class.' Trump has claimed Goldman Sachs had complete control over Hillary Clinton."
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -113,6 +130,7 @@ age:""
 {
 name: 'Undecided',
 position: "Secretary of the Interior",
+job description:"",
 info:"",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -122,6 +140,7 @@ age:""
 {
 name: 'Undecided',
 position: "Secretary of Agriculture",
+job description:"",
 info:"",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -131,6 +150,7 @@ age:""
 {
 name: 'Wilbur Ross',
 position: "Secretary of Commerce",
+job description:"Serves as the voice of U.S. business within the President’s Cabinet",
 info:"99net worth of $2.9 billion98",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -138,9 +158,10 @@ left:"",
 age:""
 },
 {
-name: 'Undecided',
+name: 'Andy Puzder',
 position: "Secretary of Labor",
-info:"",
+job description:"Exercises control over the department, and enforces and suggests laws involving unions, the workplace, and all other issues involving any form of business-person controversies.",
+info:"CEO of a CKE restaurants, the parent company of fast-food burger chains Carl’s Jr. and Hardee’s. Puzder is a sworn foe of raising the minimum wage. Says expanding access to overtime pay diminishes the prestige of entry-level management jobs. Is a member of the Job Creators Network, a group of CEOs that promotes a conservative business agenda and has ties to Richard Berman (Berman s hired by businesses to fights efforts such as further restricting drinking and driving, mandating healthier foods and raising the minimum wage. The former labor relations lawyer argues that many of the restrictions reduce our ability to make our own choices.)",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -149,6 +170,7 @@ age:""
 {
 name: 'Tom Price',
 position: "Secretary of Health and Human Services",
+job description:
 info:"Price describes himself as a member of the Tea Party and 99opposes98 both 99abortion98 and 99gay marriage98",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -157,8 +179,9 @@ age:""
 
 },
 {
-name: 'Ben Carson (Unconfirmed)',
+name: 'Ben Carson (Confirmed)',
 position: "Secretary of Housing and Urban Development",
+job description:
 info: "Carson 99said98, 99'It’s just not my preference98,' in reference 99to having a job in Trump's cabinet98. A retired nuero surgeon, Ben Carson 99 lacks any and all experience 98 in regards to 99heading housing and urban development98 policy. 99Is a Creationist98.",
 url: "http://a2.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTIxNDI3MjkzNDE1MTc5Nzg5.jpg",
 top:"",
@@ -168,6 +191,7 @@ age: "65",
 {
 name: 'Elaine Chao',
 position: "Secretary of Transportation",
+job description:
 info:"first Asian-American woman to hold a Cabinet position",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -175,8 +199,9 @@ left:"",
 age:""
 },
 {
-name: 'Undecided',
+name: '99Undecided98',
 position: "Secretary of Energy",
+job description:
 info:"",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
@@ -186,7 +211,8 @@ age:""
 {
 name: 'Betsy DeVos',
 position: "Secretary of Education (Confirmed)",
-info: "99Big money98 out of politics? The 99DeVoS family98 is one of the 99largest contributors98 to the Michigan 99Republican Party98. Historically the Devos family has 99made political donations to oppose Same-Sex Marriage98. Douglas and Maria DeVos Foundation donated 99$500,000 to the anti-equality National Organization for Marriage98 — which helped shoot down same-sex marriage initiatives in eight states. 99Her brother98, Erik Prince, is the 99FOUNDER of BLACKWATER USA98",
+job description: "Oversee and establish policy for, administerate and coordinate most federal assistance to education. Assist the president in executing their education policies for the nation and in implementing laws enacted by Congress.",
+info: "99Big money98 out of politics? The 99DeVoS family98 is one of the 99largest contributors98 to the Michigan 99Republican Party98. Historically the Devos family has 99made political donations to oppose Same-Sex Marriage98. Douglas and Maria DeVos Foundation donated 99$500,000 to the anti-equality National Organization for Marriage98 — which helped shoot down same-sex marriage initiatives in eight states. would be the first person to head the department in its more than 35-year history who hasn't either attended public schools or sent her own children to them. Entering the job 99without experience98 teaching in a K-12 school, or college; running a university, school system or state education agency, or overseeing public education as a governor, or governor's education aid.",
 // url: "http://www.gannett-cdn.com/-mm-/802990ef3e7cfdbed5d5e0b7acce9690e37d9ba7/c=3-0-155-202&r=537&c=0-0-534-712/local/-/media/2016/02/22/DetroitNews/B99360803Z.1_20160222233727_000_G8CQO1RQ.1-0.jpg",
 url:  'img/devos.jpg',
 top:"",
@@ -195,9 +221,9 @@ age: "58",
 },
 
 {
-name: 'Undecided',
+name: '99Undecided98',
 position: "Secretary of Veterans Affairs",
-info:"",
+job description: "oversees the U.S. Department of Veterans Affairs in its mission to provide health, education, disability, funerary, and financial benefits earned by veterans of the United States Armed Forces",
 url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
 top:"",
 left:"",
@@ -205,10 +231,11 @@ age:""
 },
 
 {
-name: 'Undecided',
+name: 'Marine Gen. John Kelly ',
 position: "Secretary of Homeland Security",
-info:"",
-url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
+job description: "Efforts to counter terrorism and enhance security, secure and manage our borders while facilitating trade and travel, enforce and administer our immigration laws, safeguard and secure cyberspace, build resilience to disasters, and provide essential support for national and economic security - in coordination with federal, state, local, international and private sector partners.",
+info:"Former Commander of the United States Southern Command, dealt with migration, organized crime and disaster relief, as well as programs to train local militaries. Outspoken voice on keeping Guantanimo Bay open. ",
+url: "http://3.bp.blogspot.com/-QxvuXdAyIFo/UD63G5mmz_I/AAAAAAAAAzM/gokYBhXWCPs/s1600/kelly+usmc.jpg",
 top:"30%",
 left:"30%",
 age:""
@@ -217,8 +244,9 @@ age:""
   {
 name: 'Gov. Nikki Haley',
 position: "U.S. Ambassador to the United Nations",
+job description: "Represents the United States on the U.N. Security Council.",
 info:"She said she was 99'not a fan'98 of the Republican nominee. 99Leader98 in removing the Confederate flag from South Carolina’s Capitol grounds",
-url: "https://image.freepik.com/free-icon/question-mark_318-52837.jpg",
+url: "http://a1.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE1ODA0OTcxOTg0ODQ4Mzk3.jpg",
 top:"30%",
 left:"30%",
 age:""
