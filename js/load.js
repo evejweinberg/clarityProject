683d54279888276c343f4612af482a70fa7ec65b
Evejweinberg <evejweinberg@gmail.com>
2016-12-13 11:52:08 -0500

    added video parsing and clear button

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@
 
 
 var colors = ["#a63251", "#010540", "#65a6bf", "#f2d9ba","#f2695c","#5e6083"]
-
+var outerRowMama, lrgHead;
 
 window.onload = function(){
   DrawData(people)
@@ -16,30 +16,21 @@ function DrawData(data) {
   for (var i in data){
 
     //create a div for each image, give it the class so it gets styled from the css
-    var outerRowMama = document.getElementById('Mama')
+    outerRowMama = document.getElementById('Mama')
+    outerRowMama.style.position = 'relative'
     var firstChild = document.createElement('div')
+    // firstChild.style.position = 'absolute'
     firstChild.className = "col-sm-2";
     firstChild.className += " first-child";
     var PortfolioItem = document.createElement('div')
     PortfolioItem.className = "portfolio-item";
-    // PortfolioItem.className = "col-xs-2";
-    // PortfolioItem.className += " tight-grid";
-    // divColumn.appendChild(PortfolioItem)
-    //add it ot the body
+    PortfolioItem.style.backgroundImage = "url("+data[i].url+")"
+    PortfolioItem.className += " inside";
     firstChild.appendChild(PortfolioItem)
     outerRowMama.appendChild(firstChild);
-    // PortfolioItem.className = "head";
-    // PortfolioItem.id = 'div'+i
-    // PortfolioItem.position = 'absolute'
-    // PortfolioItem.style.left = i*50 + 'px'
 
+    PortfolioItem.id = i
 
-
-    var img = document.createElement('img')
-    img.src = data[i].url
-    img.className = "inside"
-    img.id = i
-    PortfolioItem.appendChild(img)
 
 
     var string = "''"+ data[i].info+ "''"
@@ -49,71 +40,92 @@ function DrawData(data) {
 
     //create a div for the info of that person
     var divdescr = document.createElement('div');
-    divdescr.innerHTML =  '<strong>'+ data[i].position + '</strong><br>' + data[i].name +  '<br>'+ newstring2;
+    divdescr.innerHTML =  '<strong>'+ data[i].position + '</strong><br>' + data[i].name ;
+
+    // divdescr.innerHTML =  '<strong>'+ data[i].position + '</strong><br>' + data[i].name +  '<br>'+ newstring2;
     divdescr.className = "description";
     divdescr.style.color = colors[i%colors.length];
     divdescr.style.display = "none"
     divdescr.id='descr0'+i
     outerRowMama.appendChild(divdescr);
 
+    // hero image
+    lrgHead = document.createElement('div');
+    outerRowMama.appendChild(lrgHead);
+    lrgHead.className = 'lrgHead col-sm-4'
+
+
+
+
+
+
+
 
     var elems = $('.inside');
-    // var texts = $('.adjectives');
-    //if any image is rolled over
+    var texts = $('.description');
+    // //if any image is rolled over
     elems.on('mouseenter', function(e) {
-      // console.log(this)
     var num = this.id
         for (var i in people){
           if (i != num){
             $('#descr0'+i).fadeOut(10)
           } else {
+
             $('#descr0'+i).fadeIn()
           }
 
         }
 
-
-
     });
 
-//     $('.portfolio-item').on('click', function() {
-//     $(this).toggleClass('change');
-// });
-
-    $('.portfolio-item').click(function(e){
-      // $(e.target ).offsetParent().css( "border", "2px solid green" );
-      var targetObject = $(e.target).parent().parent()
-      var otherTargets = $(e.target).parent().parent().siblings();
-      var tween = TweenLite.to(targetObject, 1, {left:"632px", scale: "2", ease:Linear.easeNone});
-      var othertween = TweenLite.to(otherTargets,1,{left:"0px", scale: "1"})
-      console.log(targetObject)
-      tween.play()
-      othertween.play();
-    // $(e.target).parent().parent().addClass('change');
-    // $(e.target).parent().parent().siblings().removeClass('change')
-    videoRequest()
-  });
 
 
 
 
 } //for loop over
 
+$('#clear-button').click(function(e){
+  console.log('clear was clicked')
+  lrgHead.style.backgroundImage = ''
+  $('#mainBox').removeClass('grey-border')
+  $('#videoBox').fadeOut()
+  $('#clear-button').hide();
+
+
+})
+
+$('.portfolio-item').click(function(e){
+  // console.log(e.target.style.backgroundImage)
+  lrgHead.style.backgroundImage = e.target.style.backgroundImage
+
+  $('#mainBox').addClass('grey-border')
+  videoRequest()
+});
+
+
 } //function over
 
 
+
+
+
 function videoRequest(parameter1, parameter2){
+  $('#videoBox').fadeIn()
+  $('#clear-button').show();
+  var videString = Tempdata.response.docs[Math.floor(Math.random()*10)].identifier
+  console.log(videString)
+  document.getElementById('videoBox').src="http://archive.org/embed/"+videString;
   //parse this string with dynamic parameters
-  var jsonURL = "https://archive.org/advancedsearch.php?q=%28format%3AMPEG4+OR+format%3Ah.264%29+AND+something&fl%5B%5D=identifier&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&callback=callback"
-  $.ajax({
-    url: jsonURL,
-    type: 'GET',
-    dataType: 'json'
-  }).done(function(data) {
-    //parse the results from here and get the first identifier string,
-    //push a new iframe element to the page
-    //http://archive.org/embed/IDENTIFIER
-    //<iframe src="https://archive.org/embed/commute" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`
-    console.log(data)
-  });
+  // var jsonURL = "https://archive.org/advancedsearch.php?q=%28format%3AMPEG4+OR+format%3Ah.264%29+AND+something&fl%5B%5D=identifier&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json"
+  // $.ajax({
+  //   url: jsonURL,
+  //   type: 'GET',
+  //   dataType: 'json'
+  // }).done(function(data) {
+  //   //parse the results from here and get the first identifier string,
+  //   //push a new iframe element to the page
+  //   //http://archive.org/embed/IDENTIFIER
+  //   //<iframe src="https://archive.org/embed/commute" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>`
+  //   console.log(data)
+  // });
 }
---
+++ js/testCallback.js
@@ -0,0 +1,170 @@
+var Tempdata = {
+"responseHeader": {
+"status": 0,
+"QTime": 61,
+"params": {
+"q": "( format:MPEG4 OR format:h.264 ) AND (title:something^100 OR description:something^15 OR collection:something^10 OR language:something^10 OR text:something^1)",
+"qin": "(format:MPEG4 OR format:h.264) AND something",
+"fl": "identifier",
+"wt": "json",
+"rows": "50",
+"start": 0
+}
+},
+"response": {
+"numFound": 13653,
+"start": 0,
+"docs": [
+{
+"identifier": "I.Origins.2014.HDRip.XviDIFT"
+},
+{
+"identifier": "TheStupidShowSomethingDirty"
+},
+{
+"identifier": "See_Something._Say_Something."
+},
+{
+"identifier": "7DayWeekendSomethingElse"
+},
+{
+"identifier": "neverending3"
+},
+{
+"identifier": "Something_1"
+},
+{
+"identifier": "el-something"
+},
+{
+"identifier": "BatmanGeek-Something354"
+},
+{
+"identifier": "something_201412"
+},
+{
+"identifier": "Something_Stupid_1"
+},
+{
+"identifier": "something_important...."
+},
+{
+"identifier": "algosomething"
+},
+{
+"identifier": "Pjkproductions-somethingDeep112"
+},
+{
+"identifier": "Something_Different"
+},
+{
+"identifier": "Pjkproductions-somethingDeep195"
+},
+{
+"identifier": "Something_Cowboy_1"
+},
+{
+"identifier": "WillWoolfSomethingHadChanged"
+},
+{
+"identifier": "Era-LookingForSomething"
+},
+{
+"identifier": "SAYSOMETHINGWITTY_201601"
+},
+{
+"identifier": "SomethingForNothing"
+},
+{
+"identifier": "DoubleACS_Something1811"
+},
+{
+"identifier": "DoubleACS_Something2211"
+},
+{
+"identifier": "DoubleACS_Something0814"
+},
+{
+"identifier": "DoubleACS_Something1411"
+},
+{
+"identifier": "DoubleACS_Something1112"
+},
+{
+"identifier": "DoubleACS_Something0913"
+},
+{
+"identifier": "DoubleACS_Something1314"
+},
+{
+"identifier": "DoubleACS_Something1511"
+},
+{
+"identifier": "DoubleACS_Something1613"
+},
+{
+"identifier": "DoubleACS_Something1912"
+},
+{
+"identifier": "DoubleACS_Something1911"
+},
+{
+"identifier": "Popcornarchive-somethingHesHiding"
+},
+{
+"identifier": "DoubleACS_Something1414"
+},
+{
+"identifier": "DoubleACS_Something0313"
+},
+{
+"identifier": "DoubleACS_Something1612"
+},
+{
+"identifier": "DoubleACS_Something1413"
+},
+{
+"identifier": "DoubleACS_Something0914"
+},
+{
+"identifier": "DoubleACS_Something0813"
+},
+{
+"identifier": "DoubleACS_Something1412"
+},
+{
+"identifier": "DoubleACS_Something0113"
+},
+{
+"identifier": "DoubleACS_Something1212"
+},
+{
+"identifier": "DoubleACS_Something0114"
+},
+{
+"identifier": "DoubleACS_Something0513"
+},
+{
+"identifier": "DoubleACS_Something0112"
+},
+{
+"identifier": "DoubleACS_Something1712"
+},
+{
+"identifier": "DoubleACS_Something0412"
+},
+{
+"identifier": "DoubleACS_Something2311"
+},
+{
+"identifier": "SomethingToThinkAbout"
+},
+{
+"identifier": "Popcornarchive-somethingHesHiding_3bawd"
+},
+{
+"identifier": "DoubleACS_Something2013"
+}
+]
+}
+}
